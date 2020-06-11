80b6cd896669bb53fae1c7ff5b66813ad4d12032
JuanManuelGeny <juanmanuel.geny@gmail.com>
2020-06-11 19:18:03 -0300

    Arrancamos con Mi Registro v7

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        $useradmin = User::where('email','admin@admin.com')->first();
+    	if ($useradmin) {
+    		$useradmin->delete();
+    	}
+    	$useradmin = User::create([
+			'name' => 'admin',
+	        'email' => 'admin@admin.com',
+	        'password' => Hash::make('admin')
+    	]);
+    }
+}
